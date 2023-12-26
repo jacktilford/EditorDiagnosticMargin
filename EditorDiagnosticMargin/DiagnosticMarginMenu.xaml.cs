885cd7af9a40ba0c0920ef4d17787385fd8e7bc3
Jack Tilford <jack.tilford@hotmail.com>
2023-12-26 11:19:34 -0800

    Initial skeleton.

---
+++ EditorDiagnosticMargin/DiagnosticMarginMenu.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.VisualStudio.Text.Editor;
+
+namespace EditorDiagnosticMargin
+{
+    public partial class DiagnosticMarginMenu : UserControl, IWpfTextViewMargin
+    {
+        public const string MarginName = "DiagnosticMarginMenu";
+
+        public DiagnosticMarginMenu()
+        {
+            InitializeComponent();
+        }
+
+        public FrameworkElement VisualElement => this;
+
+        public ITextViewMargin GetTextViewMargin(string marginName)
+        {
+            return string.Compare(marginName, DiagnosticMarginMenu.MarginName, StringComparison.OrdinalIgnoreCase) == 0 ? this : (ITextViewMargin)null;
+        }
+
+        public double MarginSize => 100;
+
+        public bool Enabled => true;
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ EditorDiagnosticMargin/DiagnosticMarginMenuFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Composition;
+using EditorDiagnosticMargin;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+[Export(typeof(IWpfTextViewMarginProvider))]
+[Name(DiagnosticMarginMenu.MarginName)]
+[Order(Before = PredefinedMarginNames.HorizontalScrollBarContainer, After = "ZoomControlMarginProvider" /*PredefinedMarginNames.ZoomControl) FUCK */)]
+[MarginContainer(PredefinedMarginNames.BottomControl)]
+[ContentType("text")]
+[TextViewRole(PredefinedTextViewRoles.Interactive)]
+
+internal sealed class DiagnosticMarginMenuFactory : IWpfTextViewMarginProvider
+{
+    public IWpfTextViewMargin CreateMargin(IWpfTextViewHost textViewHost, IWpfTextViewMargin containerMargin)
+    {
+        return new DiagnosticMarginMenu();
+    }
+}
